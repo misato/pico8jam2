d9e9b221d41a8e708c15555648712f4f08ac94d2
David Eriksson <jayminer@gmail.com>
2016-05-23 15:03:24 +0200

    Added some simple error-checking to parse.py to check if a file exists.

--- parse.py
+++ parse.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-import os
+import os.path
 
 if len(sys.argv) != 2:
     sys.exit("Usage: " + os.path.basename(sys.argv[0]) + " <sourcefile>")
+
+if not os.path.exists(sys.argv[1]):
+    sys.exit("File " + sys.argv[1] + " not found.")
 
 sourceFile = file(sys.argv[1])
 for line in sourceFile:
